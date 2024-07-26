58083cb89c5be08f37c867cf84fc29de61b1bc02
pedro <phavilat@hotmail.com>
2024-07-25 22:33:33 -0300

    continuacao trabalho

--- app/src/main/java/com/example/progressbar/MainActivity.java
+++ app/src/main/java/com/example/progressbar/MainActivity.java
@@ -1,27 +1,32 @@
 package com.example.progressbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ProgressBar progressBarHorizontal;
-    private TextView textView;
+    private TextView tvGuessWord;
+    private TextView tvFeedback;
+    private EditText editTextTriedLetter;
     private int progress;
-    private String chosedWord;
+    private String guessWord;
     private String wordPlaceholder;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         progressBarHorizontal = findViewById(R.id.progressBarHorizontal);
-        textView = findViewById(R.id.textView);
+        tvGuessWord = findViewById(R.id.guessWord);
+        tvFeedback = findViewById(R.id.feedbackText);
+        editTextTriedLetter = findViewById(R.id.userInput);
 
         String[] words = {
                 "casa", "bola", "livro", "mesa", "luz", "dedo", "roda", "chuva", "festa", "gato",
@@ -31,22 +36,61 @@ public class MainActivity extends AppCompatActivity {
                 "jardim", "jogador", "pintura", "cachorro", "girafa", "elefante", "tigre", "leao", "zebra", "crocodilo"
         };
         int randomIndex = (int) (Math.random() * words.length);
-        chosedWord = words[randomIndex];
+        guessWord = words[randomIndex].toLowerCase();
 
         wordPlaceholder = "";
-        for (int i=0; i<chosedWord.length(); i++){
-            if (i<chosedWord.length()-1){
-                wordPlaceholder += "_ ";
+        for (int i = 0; i<guessWord.length(); i++){
+            wordPlaceholder += "_ ";
+        }
+        tvGuessWord.setText(wordPlaceholder);
+    }
+
+    public static boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch(NumberFormatException e){
+            return false;
+        }
+    }
+
+    public static String setPlaceholder(@NonNull String guessWord, String triedLetter, String oldPlaceholder){
+        String updatedPlaceholder = "";
+        int helperIndexForPlaceholder;
+        String letterInWord;
+        String valueInPlaceholder;
+        for (int i=0; i<guessWord.length(); i++){
+            helperIndexForPlaceholder = i*2 - 1;
+            valueInPlaceholder = String.valueOf(oldPlaceholder.charAt(helperIndexForPlaceholder));
+            letterInWord = String.valueOf(guessWord.charAt(i));
+            if (letterInWord.equals(triedLetter)){
+                updatedPlaceholder += triedLetter + " ";
+            } else if (!valueInPlaceholder.equals("_")) {
+                updatedPlaceholder += valueInPlaceholder + " ";
             } else {
-                wordPlaceholder += "_";
+                updatedPlaceholder += "_ ";
             }
         }
-
-        textView.setText(wordPlaceholder);
-
+        return updatedPlaceholder;
     }
 
     public void tryLetter(View view){
+        String triedLetter = editTextTriedLetter.getText().toString().toLowerCase();
+
+        if (isNumeric(triedLetter)) {
+            tvFeedback.setText("Insira uma letra válida!");
+            return;
+        }
+
+        if (guessWord.contains(triedLetter)){
+            wordPlaceholder = setPlaceholder(guessWord, triedLetter, wordPlaceholder);
+            tvGuessWord.setText(wordPlaceholder);
+            return;
+        }
+
+        tvFeedback.setText("Não há essa letra");
+        progress ++;
+        progressBarHorizontal.setProgress(progress);
 
     }
 }
